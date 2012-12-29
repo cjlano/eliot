cjlano/eliot

commit bedcc561f362367bce41b7ee207c0f28c36400da

Coord: do not accept invalid strings starting with valid coordinates

game/coord.cpp
@@ -100,6 +100,9 @@ void Coord::setFromString(const wstring &iWStr)
     int row = toupper(*l) - 'A' + 1;
     setCol(col);
     setRow(row);
+    // Input such as "A12foo#bar&stuff" should be invalid
+    if (isValid() && toString() != iWStr)
+        setCol(-1);
 }
 
 wstring Coord::toString(coord_mode_t mode) const

commit dc3535c953c8f585517cd1b2642a459c0a4af820

Coord: do not refuse lowercase coordinates.
It fixes a regression introduced in bedcc56.

game/coord.cpp
@@ -94,7 +94,7 @@ void Coord::setFromString(const wstring &iWStr)
     setCol(col);
     setRow(row);
     // Input such as "A12foo#bar&stuff" should be invalid
-    if (isValid() && toString() != iWStr)
+    if (isValid() && toString() != toUpper(iWStr))
         setCol(-1);
 }
 